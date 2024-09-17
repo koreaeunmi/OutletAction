42dc2fc430bfabfdc5d3a796da691b1ffed2d8c9
koreaeunmi <koreaeunmi@yahoo.com>
2024-09-17 16:23:51 -0400

    Initial Commit

---
+++ OutletAction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutletAction
+//
+//  Created by Jong Cheol Jeong on 9/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
